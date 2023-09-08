83d9dd81773f7935f81ed097e9fe8db083d63cfe
Tyuiu.SolievAH@outlook.com <katagawa.k@gmail.com>
2023-09-09 00:18:25 +0500

    Task2

---
+++ Tyuiu.SolievAH.Sprint0.Task2.V0.Test/Tyuiu.SolievAH.Sprint0.Task2.V0.Test.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../Tyuiu.SolievAH.Sprint0/Tyuiu.SolievAH.Sprint0.Task2.V0.Lib/Tyuiu.SolievAH.Sprint0.Task2.V0.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+				ISprint0Task2V0* service = new Service();
+				int a = 3;
+				int b = 4;
+				int c;
+
+				c = service->Add(a, b);
+
+				Assert::AreEqual(7, c);
+		}
+	};
+}
